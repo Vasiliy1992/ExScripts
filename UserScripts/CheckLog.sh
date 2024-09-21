e67ba0890d5b9d3ad31d8b8473735ac554514b43
Vasiliy1992 <vas199228@gmail.com>
2024-09-21 12:19:56 +0000

    Edit CheckLog.sh

--- UserScripts/CheckLog.sh
+++ UserScripts/CheckLog.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
-#Reading the latest log
+
+# Path to the logs folder
 log_path=~/RMS_data/logs/RMS_logs
-log=$(ls -t $log_path/* | head -1)
-echo ""
-echo "Open current log:"
-echo $log
-echo "============================================================"
-echo ""
-tail -f $log
+
+# Number of log lines to output
+N=100
+
+
+# Get the full path to the last created log
+log=$(\
+	ls --sort=time $log_path/* \
+	| head -1\
+)
+
+
+# Display information about the current log
+clear
+echo -e "\n\tOpen current log:
+\n\t$log
+\t======================================================================\n"
+
+
+# Display current N log lines
+tail \
+	--follow \
+	--lines=$N \
+	$log
+
