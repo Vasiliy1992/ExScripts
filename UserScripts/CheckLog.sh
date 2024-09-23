c14de98c6ffd49bcae3dcf8190e5050e929cda07
Vasiliy1992 <vas199228@gmail.com>
2024-09-23 00:26:16 +0000

    Added .config parsing

--- UserScripts/CheckLog.sh
+++ UserScripts/CheckLog.sh
@@ -1,29 +1,52 @@
 #!/bin/bash
 
-# Path to the logs folder
-log_path=~/RMS_data/logs/RMS_logs
-
-# Number of log lines to output
+# Number of lines of the current log to display
 N=100
 
 
-# Get the full path to the last created log
-log=$(\
-	ls --sort=time $log_path/* \
-	| head -1\
-)
+get_log_dir(){
+	log_fold="$HOME/RMS_data/"$(awk \
+					'/^log_dir:/{print $2}'\
+					 $HOME/source/RMS/.config\
+	)
+	echo $log_fold
+}
 
 
-# Display information about the current log
-clear
-echo -e "\n\tOpen current log:
-\n\t$log
-\t======================================================================\n"
+get_current_log(){
+	log=$(\
+		ls --sort=time $log_fold/* \
+		| head -1\
+	)
+	echo $log
+}
 
 
-# Display current N log lines
-tail \
-	--follow \
-	--lines=$N \
-	$log
+disp_info(){
+	clear
+	echo -e "\n\tOpen current log:
+	\n\t$log
+	======================================================================\n"
+}
 
+
+# Display last N lines of current log
+disp_log_lines(){
+	tail \
+		--follow \
+		--lines=$N \
+		$log
+}
+
+
+main(){
+	get_log_dir
+	get_current_log
+	disp_info
+	disp_log_lines
+}
+
+
+main
+
+exit 0
