06bae96e7bc1ac2090054d0eca7450e74597ed81
Vasiliy1992 <vas199228@gmail.com>
2024-09-23 13:10:20 +0000

    Rewrite ./UpArchives/UpArchives.sh

--- UpArchives/UpArchives.sh
+++ UpArchives/UpArchives.sh
@@ -2,8 +2,32 @@
 
 source $(dirname $0)/.uparchives.cfg
 
-date
-rsync -av --protocol=29 $HOME/RMS_data/ArchivedFiles/*.tar.bz2 $SERVER:/home/srmn/archives/$ID
-#rsync -av #HOME/RMS_data/ArchivedFiles/*.tar.bz2 $SERVER:/home/srmn/archives/$ID
-rsync -av $HOME/RMS_data/*_fits_counts.txt $SERVER:/home/srmn/reports
 
+up_archive(){
+	rsync \
+		--archive \
+		--verbose \
+		--protocol=29 \
+				$HOME/RMS_data/ArchivedFiles/*.tar.bz2 \
+				$SERVER:/home/srmn/archives/$ID
+}
+
+
+up_report(){
+	rsync \
+		--archive \
+		--verbose \
+			$HOME/RMS_data/*_fits_counts.txt \
+			$SERVER:/home/srmn/reports
+}
+
+
+main(){
+	date
+	up_archive
+	up_report
+}
+
+main
+
+exit 0
