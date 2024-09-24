6e0ba101290a3830afba890eadc8b37afeaf2baa
Vasiliy1992 <vas199228@gmail.com>
2024-09-24 07:10:30 +0000

    Fix ./MainExScripts/UpgradeExScripts.sh

--- MainExScripts/UpgradeExScripts.sh
+++ MainExScripts/UpgradeExScripts.sh
@@ -3,22 +3,21 @@
 # Script for updates all scripts from GitHub repository/
 
 SAVE_CONFIG_FOLDER=$HOME/.ExScriptsConfigs
-SCRIPT_FOLDER=$(dirname $0)
 
 
 save_configs() {
 	echo "Step 1 of 3. Save configs"
-	mkdir $SAVE_CONFIG_FOLDER 2>/dev/null
-	cp "$SCRIPT_FOLDER/Starvisor/.starvisor.cfg" $SAVE_CONFIG_FOLDER
-	cp "$SCRIPT_FOLDER/UpArchives/.uparchives.cfg" $SAVE_CONFIG_FOLDER
-	cp "$SCRIPT_FOLDER/UploadCSV/.up_csv.cfg" $SAVE_CONFIG_FOLDER
+	mkdir --parents $SAVE_CONFIG_FOLDER
+	cp --preserve --update $HOME/source/ExScripts/Starvisor/.starvisor.cfg $SAVE_CONFIG_FOLDER
+	cp --preserve --update $HOME/source/ExScripts/UpArchives/.uparchives.cfg $SAVE_CONFIG_FOLDER
+	cp --preserve --update $HOME/source/ExScripts/UploadCSV/.up_csv.cfg $SAVE_CONFIG_FOLDER
 }
 
 
 update_scripts() {
 	echo "Step 2 of 3. Upgrade scripts."
 	# Go to the script folder
-	cd $(dirname $0)
+	cd $HOME/source/ExScripts
 	# Stash the cahnges
 	git stash
 	# Pull new code from github
@@ -30,9 +29,9 @@ update_scripts() {
 
 return_configs() {
 	echo "Step 3 of 3. Return configs"
-	cp --force "$SAVE_CONFIG_FOLDER/.starvisor.cfg" "$SCRIPT_FOLDER/Starvisor"
-	cp --force "$SAVE_CONFIG_FOLDER/.uparchives.cfg" "$SCRIPT_FOLDER/UpArchives"
-	cp --force "$SAVE_CONFIG_FOLDER/.up_csv.cfg" "$SCRIPT_FOLDER/UploadCSV"
+	cp --preserve --update $SAVE_CONFIG_FOLDER/.starvisor.cfg $HOME/source/ExScripts/Starvisor
+	cp --preserve --update $SAVE_CONFIG_FOLDER/.uparchives.cfg $HOME/source/ExScripts/UpArchives
+	cp --preserve --update $SAVE_CONFIG_FOLDER/.up_csv.cfg $HOME/source/ExScripts/UploadCSV
 }
 
 
