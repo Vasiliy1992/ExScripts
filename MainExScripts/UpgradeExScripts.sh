eca56c7e30a9f33b5e3570ad148c27eee84bbd4a
Vasiliy1992 <vas199228@gmail.com>
2024-09-24 07:35:56 +0000

    Fix ./MainExScripts/UpgradeExScripts.sh

--- MainExScripts/UpgradeExScripts.sh
+++ MainExScripts/UpgradeExScripts.sh
@@ -8,9 +8,18 @@ SAVE_CONFIG_FOLDER=$HOME/.ExScriptsConfigs
 save_configs() {
 	echo "Step 1 of 3. Save configs"
 	mkdir --parents $SAVE_CONFIG_FOLDER
-	cp --preserve --update $HOME/source/ExScripts/Starvisor/.starvisor.cfg $SAVE_CONFIG_FOLDER
-	cp --preserve --update $HOME/source/ExScripts/UpArchives/.uparchives.cfg $SAVE_CONFIG_FOLDER
-	cp --preserve --update $HOME/source/ExScripts/UploadCSV/.up_csv.cfg $SAVE_CONFIG_FOLDER
+	cp $HOME/source/ExScripts/Starvisor/.starvisor.cfg $SAVE_CONFIG_FOLDER
+	cp $HOME/source/ExScripts/UpArchives/.uparchives.cfg $SAVE_CONFIG_FOLDER
+	cp $HOME/source/ExScripts/UploadCSV/.up_csv.cfg $SAVE_CONFIG_FOLDER
+}
+
+
+chk_save_config_fold(){
+        if ! [ -d $SAVE_CONFIG_FOLDER ]; then
+                save_configs
+        else
+		echo -e "Step 1 of 3. Check folder:\n$SAVE_CONFIG_FOLDER\nFolder exist!"
+	fi
 }
 
 
@@ -29,15 +38,15 @@ update_scripts() {
 
 return_configs() {
 	echo "Step 3 of 3. Return configs"
-	cp --preserve --update $SAVE_CONFIG_FOLDER/.starvisor.cfg $HOME/source/ExScripts/Starvisor
-	cp --preserve --update $SAVE_CONFIG_FOLDER/.uparchives.cfg $HOME/source/ExScripts/UpArchives
-	cp --preserve --update $SAVE_CONFIG_FOLDER/.up_csv.cfg $HOME/source/ExScripts/UploadCSV
+	cp $SAVE_CONFIG_FOLDER/.starvisor.cfg $HOME/source/ExScripts/Starvisor
+	cp $SAVE_CONFIG_FOLDER/.uparchives.cfg $HOME/source/ExScripts/UpArchives
+	cp $SAVE_CONFIG_FOLDER/.up_csv.cfg $HOME/source/ExScripts/UploadCSV
 }
 
 
 main() {
 	echo "Upgrade external scripts"
-	save_configs
+	chk_save_config_fold
 	update_scripts
 	return_configs
 	echo -e "\n"
