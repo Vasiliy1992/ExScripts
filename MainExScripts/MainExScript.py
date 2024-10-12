9abbbfacea1d85b96633a2e2f7828bae01bf4591
Vasiliy1992 <vas199228@gmail.com>
2024-10-12 13:02:49 +0000

    Fix MainExScripts.py

--- MainExScripts/MainExScript.py
+++ MainExScripts/MainExScript.py
@@ -6,6 +6,6 @@ import os
 
 def rmsExternal(cap_dir, arch_dir, config):
     # Update ExScripts
-#    os.system('~/source/ExScripts/MainExScripts/UpgradeExScripts.sh')
+    os.system('~/source/ExScripts/MainExScripts/UpgradeExScripts.sh')
     # Run ExScripts
     os.system('~/source/ExScripts/MainExScripts/MainExScript.sh {}'.format(cap_dir))
