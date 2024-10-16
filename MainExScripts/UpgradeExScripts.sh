dd95be0c5a5f2aea5945933a17085b917011d529
Vasiliy1992 <vas199228@gmail.com>
2024-10-16 08:28:26 +0000

    Added config check

--- MainExScripts/UpgradeExScripts.sh
+++ MainExScripts/UpgradeExScripts.sh
@@ -8,19 +8,37 @@ SAVE_CONFIG_FOLDER=$HOME/.ExScriptsConfigs
 
 
 save_configs() {
-	echo "Step 1 of 3. Save configs"
+	echo "Step 1 of 3. Save changes to configs"
 	mkdir --parents $SAVE_CONFIG_FOLDER
-	cp $HOME/source/ExScripts/Starvisor/.starvisor.cfg $SAVE_CONFIG_FOLDER
-	cp $HOME/source/ExScripts/UpArchives/.uparchives.cfg $SAVE_CONFIG_FOLDER
-	cp $HOME/source/ExScripts/UploadCSV/.up_csv.cfg $SAVE_CONFIG_FOLDER
+	cp $HOME/source/ExScripts/Starvisor/.starvisor.cfg $SAVE_CONFIG_FOLDER --update --preserve
+	cp $HOME/source/ExScripts/UpArchives/.uparchives.cfg $SAVE_CONFIG_FOLDER --update --preserve
+	cp $HOME/source/ExScripts/UploadCSV/.up_csv.cfg $SAVE_CONFIG_FOLDER --update --preserve
 }
 
 
 chk_save_config_fold(){
         if ! [ -d $SAVE_CONFIG_FOLDER ]; then
-                save_configs
+                echo -e "\nERROR! \nFolder: \n$SAVE_CONFIG_FOLDER\nnot exist!"
+		exit 0
         else
-		echo -e "Step 1 of 3. Check folder:\n$SAVE_CONFIG_FOLDER\nFolder exist!"
+                echo -e "\nFolder: \n$SAVE_CONFIG_FOLDER \nexist! \nGoto step 2 of 3..."
+        fi
+}
+
+
+chk_empty_cfg() {
+	# Checking for the presence of parameters in configs
+	id=$(cat $HOME/source/ExScripts/Starvisor/.starvisor.cfg | awk -F"=" '/^ID/{print $2}' | tr --delete \")
+	ID=$(cat $HOME/source/ExScripts/UpArchives/.uparchives.cfg | awk -F"=" '/^ID/{print $2}' | tr --delete \")
+	YDtoken=$(cat $HOME/source/ExScripts/UploadCSV/.up_csv.cfg | awk -F"=" '/^YDtoken/{print $2}' | tr --delete \")
+#	echo $id $ID $YDtoken
+
+	# If there are no parameters in the config, copy from the backup folder if there is one
+	if [[ "$id" == "" || "$ID" == "" || "$YDtoken" == '' ]]; then
+		echo "Configs are empty! No saving required."
+		chk_save_config_fold
+	else
+		save_configs
 	fi
 }
 
@@ -40,15 +58,15 @@ update_scripts() {
 
 return_configs() {
 	echo "Step 3 of 3. Return configs"
-	cp $SAVE_CONFIG_FOLDER/.starvisor.cfg $HOME/source/ExScripts/Starvisor
-	cp $SAVE_CONFIG_FOLDER/.uparchives.cfg $HOME/source/ExScripts/UpArchives
-	cp $SAVE_CONFIG_FOLDER/.up_csv.cfg $HOME/source/ExScripts/UploadCSV
+	cp $SAVE_CONFIG_FOLDER/.starvisor.cfg $HOME/source/ExScripts/Starvisor  --preserve
+	cp $SAVE_CONFIG_FOLDER/.uparchives.cfg $HOME/source/ExScripts/UpArchives --preserve
+	cp $SAVE_CONFIG_FOLDER/.up_csv.cfg $HOME/source/ExScripts/UploadCSV --preserve
 }
 
 
 main() {
-	echo "Upgrade external scripts"
-	chk_save_config_fold
+	echo -e "\nUpgrade external scripts\n========================================================"
+	chk_empty_cfg
 	update_scripts
 	return_configs
 	echo -e "\n"
